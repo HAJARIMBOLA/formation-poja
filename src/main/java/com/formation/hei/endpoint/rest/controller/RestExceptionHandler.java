a3f44036447fa2abf9133ba3a92c44924bb03877
Miahy03 <hei.miahy.2@gmail.com>
2026-07-03 11:55:52 +0300

    feat(exception): map subscription exceptions to 404 and 409 responses

--- src/main/java/com/formation/hei/endpoint/rest/controller/RestExceptionHandler.java
+++ src/main/java/com/formation/hei/endpoint/rest/controller/RestExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.formation.hei.endpoint.rest.controller;
 
+import com.formation.hei.exception.AlreadySubscribedException;
+import com.formation.hei.exception.CourseNotFoundException;
+import com.formation.hei.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,5 +14,15 @@ public class RestExceptionHandler {
   @ExceptionHandler(IllegalArgumentException.class)
   public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+  }
+
+  @ExceptionHandler({UserNotFoundException.class, CourseNotFoundException.class})
+  public ResponseEntity<String> handleNotFound(RuntimeException e) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+  }
+
+  @ExceptionHandler(AlreadySubscribedException.class)
+  public ResponseEntity<String> handleAlreadySubscribed(AlreadySubscribedException e) {
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
   }
 }
