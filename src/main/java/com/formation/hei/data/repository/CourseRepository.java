1e1d7e86d41d4f8747033a4d606c601f0cb8bbdc
AnahAntonerrie <andrianahanah31@gmail.com>
2026-07-06 07:12:18 +0300

    add: entites-user-course

--- src/main/java/com/formation/hei/data/repository/CourseRepository.java
+++ src/main/java/com/formation/hei/data/repository/CourseRepository.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/** Repository giving CRUD access to {@link Course} entities. */
 @Repository
 public interface CourseRepository extends JpaRepository<Course, UUID> {
 
--- src/main/java/com/formation/hei/data/repository/UserRepository.java
+++ src/main/java/com/formation/hei/data/repository/UserRepository.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/** Repository giving CRUD access to {@link User} entities. */
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
