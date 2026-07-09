5a34e09c5aac3436496c4fb3fb983fdce53277f2
Miahy03 <hei.miahy.2@gmail.com>
2026-07-09 09:28:53 +0300

    rm: package formation.hei

--- src/main/java/com/formation/hei/endpoint/rest/model/InscriptionResponse.java
+++
@@ -1,3 +0,0 @@
-package com.formation.hei.endpoint.rest.model;
-
-public record InscriptionResponse(String message) {}
--- src/main/java/com/formation/hei/file/hash/FileHash.java
+++
@@ -1,6 +0,0 @@
-package com.formation.hei.file.hash;
-
-import com.formation.hei.PojaGenerated;
-
-@PojaGenerated
-public record FileHash(FileHashAlgorithm algorithm, String value) {}
